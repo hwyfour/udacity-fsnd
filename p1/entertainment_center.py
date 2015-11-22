8a79f179c7a05b4e1e907b1bb42a106c3e349026
Dan Harrison <hwyfour@gmail.com>
2015-11-22 15:47:58 -0800

    added csv reader

--- p1/entertainment_center.py
+++ p1/entertainment_center.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import csv
+import sys
+
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie(
-    "Toy Story",
-    "A story about toys",
-    "http://ia.media-imdb.com/images/M/MV5BMTYzNzU5MzQ4OV5BMl5BanBnXkFtZTcwMDcxNDg3OA@@._V1_SY317_CR12,0,214,317_AL_.jpg",
-    "https://www.youtube.com/watch?v=XKSQmYUaIyE")
 
-movies = [toy_story]
+def main(list):
 
-fresh_tomatoes.open_movies_page(movies)
+    movies = []
 
-# If you would like to exceed specifications, add more information about the movies, such as the actors, the release date, trivia, etc. or play around with the HTML and the CSS to redo the format and styling of the page.
+    with open(list, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            movies.append(media.Movie(*row))
 
+    fresh_tomatoes.open_movies_page(movies)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print 'Usage: %s <csv with movie details>' % sys.argv[0]
--- p1/media.py
+++ p1/media.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import webbrowser
-
-
- # class video as a parent, has title, children of movie and tv show
-
 class Movie():
     """ This class stores movie information """
-
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self,
             movie_title,
             movie_storyline,
+            movie_year,
+            movie_rating,
+            movie_length,
             movie_poster_image_url,
             movie_trailer_youtube_url):
 
         self.title = movie_title
         self.storyline = movie_storyline
+        self.year = movie_year
+        self.rating = movie_rating
+        self.length = movie_length
         self.poster_image_url = movie_poster_image_url
         self.trailer_youtube_url = movie_trailer_youtube_url
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
